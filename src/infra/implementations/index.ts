712109af2715d293bf8aa4a708f64f39e135d370
Luzin7 <lvictor.dutra@gmail.com>
2024-02-08 18:37:10 -0300

    feat: add implementation

--- src/infra/implementations/index.ts
+++ src/infra/implementations/index.ts
@@ -1,8 +1,18 @@
+import { MoviesRepositoryImplementation } from "@infra/database/prisma/repositories/movies/UsersRepository";
+import { SeriesRepositoryImplementation } from "@infra/database/prisma/repositories/series/SeriesRepositoryImplementation";
 import { UsersRepositoryImplementation } from "@infra/database/prisma/repositories/users/UsersRepository";
+import CreateMovieUseCase from "@module/movies/useCase/createMovie";
+import CreateSerieUseCase from "@module/series/useCase/createSerie";
 import CreateUserUseCase from "@module/users/useCase/createUser";
 import { CryptographyProviderImplementation } from "@providers/cryptography/implementations/CryptographyProviderImplementation";
 
 export const createUserUseCase = new CreateUserUseCase(
   new UsersRepositoryImplementation(),
   new CryptographyProviderImplementation(),
+);
+export const createMovieUseCase = new CreateMovieUseCase(
+  new MoviesRepositoryImplementation(),
+);
+export const createSerieUseCase = new CreateSerieUseCase(
+  new SeriesRepositoryImplementation(),
 );
